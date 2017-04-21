3fa2cae929dcb89ce4514428db2dc39f73da9014
shidaping <shidaping@zhongan.io>
2017-04-21 19:22:22 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var spritesmith = require('gulp.spritesmith');
+
+var config={
+  src:'src/img/**/*',
+  dist:'dist/img'
+}
+gulp.task('default',function(){
+  return gulp.src(config.src)
+  .pipe(imagemin())
+  .pipe(gulp.dest(config.dist));
+})
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('src/sprite/*')
+    .pipe(imagemin())
+    .pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.css'
+  }));
+  return spriteData.pipe(gulp.dest('dist/sprite'));
+});
